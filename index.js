d8750a5e3d1674e9a06c672ee9a6315638a040b0
Pablo Hernandez <hpablo575@gmail.com>
2022-09-21 18:36:55 -0600

    added a prep time function that multiplies 2 minutes times the number of layers laid

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const EXPECTED_MINUTES_IN_OVEN = 40;
 // has been in the oven for as a parameter and returns how many minutes the lasagna stil lhas to remain in the oven, 
 // based on the expected oven time fro previous task
 function remainingMinutesInOven(actualMinutesIn) {
-    return EXPECTED_MINUTES_IN_OVEN - actualMinutesIn;
+    return  EXPECTED_MINUTES_IN_OVEN - actualMinutesIn;
+};
+console.log(remainingMinutesInOven(30));
+
+// adding function that takes the number of layers added to the lasagana 
+// as a parameter and returns how many minutes were spent preparing the lasagna,
+//assuming each layer takes 2 minutes to prepare
+function preparationTimeInMinutes(layers){
+    let layer = 2;
+    return 2 *layers;
 }
-console.log(remainingMinutesInOven(30)
-);
+console.log(preparationTimeInMinutes(4));
